5aafb0695098f1acd8ef05b4cd214337d6e9b552
Juri Leino <github@line-o.de>
2019-07-11 17:46:09 +0200

    fix (ci): re-add teardown.ps1

---
+++ ci/teardown.ps1
@@ -0,0 +1,10 @@
+cd C:/build/exist-eXist-$env:EXIST_VERSION
+
+if ($env:EXIST_VERSION -eq "2.2") {
+  & ./tools/wrapper/bin/wrapper-windows-x86-64.exe --stop ..\conf\wrapper.conf
+} else {
+  Stop-Process $env:EXIST_PROCESS
+}
+& ./build.bat clean-default-data-dir
+Remove-Item -Recurse -Force webapp/WEB-INF/logs/*
+Remove-Item -Recurse -Force tools/jetty/logs/*
